bc4b0d71aa8a271e9a70dfa4a203f34414e7a119
Appan Chhibber <appanchhibber@hotmail.com>
2017-05-30 15:43:07 -0400

    stats operation test case added

--- src/com/DESCRIPTIVE_STATISTICS/tests/StatisticOperationsTest.java
+++ src/com/DESCRIPTIVE_STATISTICS/tests/StatisticOperationsTest.java
@@ -1,5 +1,26 @@
 package com.DESCRIPTIVE_STATISTICS.tests;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import com.DESCRIPTIVE_STATISTICS.Operations.StatisticOperations;
+
 
 public class StatisticOperationsTest {
-
+	private static List<Integer> dataSet; 
+	private static StatisticOperations statsOperation;
+@BeforeClass
+public static void intializeData(){
+	Integer[] data={1,2,3,4,5,6,7,8,9,9,9,9,12,12,12,12,12,12,96};
+	dataSet=Arrays.asList(data);
+	statsOperation=new StatisticOperations();
+}
+	@Test
+	public void modeTest(){
+		String result=statsOperation.computeMode(dataSet);
+		assertEquals("12",result);
+	}
 }
